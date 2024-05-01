70ec1c660d59d4f894500f881fae3cd970649bb6
unknown <aronilie.code@gmail.com>
2024-05-01 16:32:48 +0200

    Changed publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:5200/",
+    publicPath: argv.mode === "development"
+    ? "http://localhost:5200/"
+    : "https://frontend-1byj.onrender.com",
   },
 
   resolve: {
