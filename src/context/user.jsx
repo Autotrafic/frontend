bc43113c2855e0590f889957916816fc6f3df941
unknown <aronilie.code@gmail.com>
2024-05-07 21:10:29 +0200

    Added ipdata import

--- src/context/user.jsx
+++ src/context/user.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
+import IPData from "ipdata";
 import { fetchRegisterLog } from "../services/logs";
 
 const UserContext = createContext();
