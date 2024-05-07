5f7ba9951ad9f21ce43d546392fc5a2a62431dc6
unknown <aronilie.code@gmail.com>
2024-05-07 21:07:47 +0200

    Added ipdata

--- src/context/user.jsx
+++ src/context/user.jsx
@@ -1,15 +1,25 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { fetchRegisterLog } from "../services/logs";
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
   const generateId = () => "id" + Math.random().toString(16).slice(2);
+  const ipdata = new IPData(IPDATA_KEY);
 
+  const [ipDetails, setIpDetails] = useState();
   const [userInfo, setUserInfo] = useState({ sessionId: generateId() });
 
+  useEffect(() => {
+    (async () => {
+      const receivedIpDetails = await ipdata.lookup();
+      if (receivedIpDetails) setIpDetails(receivedIpDetails);
+    })();
+  }, []);
+
   const registerLog = async (message) => {
-    await fetchRegisterLog(message, userInfo.sessionId);
+    if (userInfo.sessionId && ipDetails?.region)
+      await fetchRegisterLog(message, userInfo.sessionId, ipDetails?.region);
   };
 
   return <UserContext.Provider value={{ userInfo, registerLog }}>{children}</UserContext.Provider>;
