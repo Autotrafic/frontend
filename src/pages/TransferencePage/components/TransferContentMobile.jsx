4327aa69b768f355f04211ef3526b4d63e750f55
unknown <aronilie.code@gmail.com>
2024-07-23 14:04:50 +0300

    Updated TransferContentMobile

--- src/pages/TransferencePage/components/TransferContentMobile.jsx
+++ src/pages/TransferencePage/components/TransferContentMobile.jsx
@@ -6,70 +6,46 @@ import GoogleReviewsBox from "../../../components/reusable/GoogleReviewsBox";
 import dgtLogo from "../../../assets/dgt.svg";
 import ministerioImg from "../../../assets/ministerio.svg";
 import colegioImg from "../../../assets/colegio-simple-logo.png";
-import numberOneImg from "../../../assets/number-one.png";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { colors } from "../../../utils/styles";
 
 export default function TransferContentMobile() {
   return (
     <TransferContainer>
-      <Title fontSize={"21px"}>Cambio de nombre online</Title>
-
+      <Title fontSize={"21px"}>Cambia de nombre tu vehículo online en menos de 5 minutos</Title>
       <TextWrapper>
-        {/* <SubTitleTextWrapper>
-          <SubTitle>Tramita de forma </SubTitle>
-          <BlueSubTitle>rápida</BlueSubTitle> <SubTitle>y</SubTitle>
-          <BlueSubTitle>sencilla</BlueSubTitle>
-        </SubTitleTextWrapper> */}
-
-        <ExplanationText>
-          Transfiere tu vehículo en sólo 5 minutos sin pedir cita, sin papeleos
-          y sin riesgos.
-        </ExplanationText>
-        <ExplanationText>
-          Ofrecemos la mejor calidad en servicio, atención personalizada y
-          satisfacción del cliente
-        </ExplanationText>
+        <SubTitle>
+          Gestoría Online especializada en la Transferencia de Vehículos con la DGT
+        </SubTitle>
+        <ExplanationText>NOS ENCARGAMOS DE TODO - MEJOR PRECIO GARANTIZADO</ExplanationText>
       </TextWrapper>
 
       <BoxesContainer>
-        <img src={dgtLogo} alt="" width={75} />
-
-        <img
-          src={ministerioImg}
-          alt=""
-          width={90}
-          style={{ borderRadius: "2px" }}
-        />
+        <img src={colegioImg} alt="" width={125} style={{ borderRadius: "2px" }} />
+        <img src={dgtLogo} alt="" width={60} />
+        <img src={ministerioImg} alt="" width={90} style={{ borderRadius: "2px" }} />
       </BoxesContainer>
 
       <WhatsContactContainer>
-        <WhatsText>
-          Te guiamos por Whatsapp en todo el proceso, respondemos al instante
-        </WhatsText>
-        <a
-          href="https://wa.me/643219297"
-          target="_blank"
-          style={{ textDecoration: "none" }}
-        >
+        <WhatsText>Te guiamos por Whatsapp en todo el proceso</WhatsText>
+        <a href="https://wa.me/643219297" target="_blank" style={{ textDecoration: "none" }}>
           <WhatsLink>
             <WhatsIcon icon={faWhatsapp} size="xl" />
             Habla con un gestor ahora
           </WhatsLink>
         </a>
+        <WhatsText>Respondemos al instante</WhatsText>
       </WhatsContactContainer>
 
-      {/*  <GoogleReviewsAndPhoneButtonContainer>
-        <ContactLink
-          href="callto:+34643219297"
-          style={{ textDecoration: "none" }}
-        >
+      <GoogleReviewsAndPhoneButtonContainer>
+        <GoogleReviewsBox />
+        <ContactLink href="callto:+34643219297" style={{ textDecoration: "none" }}>
           <ContactButton>
             <ContactIcon icon={faPhone} />
             <ContactButtonText>Llama a un gestor</ContactButtonText>
           </ContactButton>
         </ContactLink>
-      </GoogleReviewsAndPhoneButtonContainer> */}
+      </GoogleReviewsAndPhoneButtonContainer>
     </TransferContainer>
   );
 }
@@ -80,12 +56,8 @@ const TransferContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.5em;
+  gap: 1.8em;
   margin-bottom: 1.5em;
-`;
-
-const SubTitleTextWrapper = styled.div`
-  display: flex;
 `;
 
 const SubTitle = styled.p`
@@ -96,54 +68,26 @@ const SubTitle = styled.p`
   text-align: center;
 `;
 
-const BlueSubTitle = styled.p`
-  font-size: 15px;
-  color: ${colors.primaryColor};
-  margin: 0;
-  font-weight: 600;
-  text-align: center;
-  margin: 0 5px;
-`;
-
-const NumberOneContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  margin-top: 5px;
-  padding: 5px 12px 5px 5px;
-  border-radius: 3px;
-  background-color: #f7f7f7;
-  box-shadow: 0px 0px 9px rgba(0, 0, 0, 0.25);
-`;
-
 const ExplanationText = styled.p`
-  font-size: 14px;
+  font-size: 13px;
   margin: 0;
   line-height: 22px;
-  color: #131313;
   text-align: center;
 `;
 
 const BoxesContainer = styled.div`
   display: flex;
-  justify-content: space-around;
+  align-items: center;
   gap: 20px;
-  margin: 1.3em 0;
-  width: 85%;
 `;
 
 const WhatsContactContainer = styled.div`
   margin: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
 
 const WhatsText = styled.p`
   text-align: center;
-  font-size: 14px;
-  margin: 0;
-  color: #505050;
+  font-size: 15px;
 `;
 
 const WhatsLink = styled.a`
@@ -151,15 +95,15 @@ const WhatsLink = styled.a`
   justify-content: center;
   align-items: center;
   text-align: center;
-  -moz-column-gap: 0.7rem;
-  column-gap: 0.7rem;
+  -moz-column-gap: 1rem;
+  column-gap: 1rem;
   border-radius: 6px;
   background-color: #51ca50;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
   color: white;
-  margin-top: 0.5rem;
-  padding: 0.4rem 1rem;
-  font-size: 17px;
+  margin: 0.5rem 0;
+  padding: 0.5rem 1.5rem;
+  font-size: 18px;
   width: -moz-fit-content;
   width: 100%;
   font-weight: 600;
@@ -171,8 +115,6 @@ const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 5px;
-  align-items: center;
-  margin-top: 15px;
 `;
 
 const GoogleReviewsAndPhoneButtonContainer = styled.div`
