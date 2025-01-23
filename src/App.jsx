be5b73ad472ec5461cba8c7fd9088835ec3d7e62
unknown <aronilie.code@gmail.com>
2025-01-23 13:45:41 +0100

    Added legal pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import "./styles/App.css";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import TransferencePage from "./pages/TransferencePage";
-import UploadDocsLaterPage from "./pages/messages/UploadDocsLaterPage";
-import NotFoundPage from "./pages/404";
-import FloatingWhatsappIcon from "./components/FloatingWhatsappIcon";
-import NavbarContainer from "./containers/NavbarContainer";
-import FooterContainer from "./containers/FooterContainer";
-import UploadedDocsPage from "./pages/messages/UploadedDocsPage";
+import './styles/App.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TransferencePage from './pages/TransferencePage';
+import UploadDocsLaterPage from './pages/messages/UploadDocsLaterPage';
+import NotFoundPage from './pages/404';
+import FloatingWhatsappIcon from './components/FloatingWhatsappIcon';
+import NavbarContainer from './containers/NavbarContainer';
+import FooterContainer from './containers/FooterContainer';
+import UploadedDocsPage from './pages/messages/UploadedDocsPage';
+import AvisoLegal from './pages/legal/AvisoLegal';
+import CondicionesUso from './pages/legal/CondicionesUso';
 
 function App() {
   return (
@@ -28,14 +30,11 @@ function Router() {
     <Routes>
       <Route path="/" element={<TransferencePage />} />
       <Route path="/referral/:referralId" element={<TransferencePage />} />
-      <Route
-        path="/documentacion-pendiente/:orderId"
-        element={<UploadDocsLaterPage />}
-      />
-      <Route
-        path="/gracias-documentacion/:orderId"
-        element={<UploadedDocsPage />}
-      />
+      <Route path="/documentacion-pendiente/:orderId" element={<UploadDocsLaterPage />} />
+      <Route path="/gracias-documentacion/:orderId" element={<UploadedDocsPage />} />
+
+      <Route path="/condiciones" element={<CondicionesUso />} />
+      <Route path="/aviso-legal" element={<AvisoLegal />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
@@ -45,5 +44,5 @@ ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
---
+++ src/pages/legal/CondicionesUso.jsx
@@ -0,0 +1,213 @@
+import styled from 'styled-components';
+
+export default function CondicionesUso() {
+  return (
+    <Container>
+      <Section>
+        <h1>Condiciones generales</h1>
+        <Condition>
+          <h2>Previo</h2>
+          <Paragraph>
+            Las presentes disposiciones determinan los términos y condiciones generales de contratación del servicio
+            titularidad de la persona física autónoma Ovidiu Sebastian Ilie (NIF:X3879058Q), en adelante AutoTrafic Gestoría,
+            con domicilio social en Camí dels Tamarius, 88, 08800 Vilanova i la Geltrú, Barcelona.
+          </Paragraph>
+        </Condition>
+
+        <Condition>
+          <h2>2. Acuerdo</h2>
+          <Paragraph>
+            2.1 Cuando el cliente contrata nuestro servicio éste acepta de forma automática y simultáneamente estos términos
+            y condiciones.
+          </Paragraph>
+          <Paragraph>
+            2.2 Se establecerá un acuerdo solamente si AutoTrafic Gestoría acepta tramitar la solicitud del cliente.
+            AutoTrafic Gestoría tiene derecho a renunciar a la tramitación de cualquier tramitación por el motivo que sea. En
+            caso de renuncia, lo notificará al cliente.
+          </Paragraph>
+          <Paragraph>
+            2.3 Al establecer un acuerdo con AutoTrafic Gestoría, el cliente garantiza que está autorizado y tiene capacidad
+            legal para establecer el acuerdo.
+          </Paragraph>
+          <Paragraph>
+            2.4 El acuerdo finaliza: Cuando el cliente recibe la documentación final de la gestión solicitada. Cuando
+            AutoTrafic Gestoría establece que la tramitación o la gestión solicitada por el cliente no es viable y advierte a
+            éste que no se continuará con dicha tramitación. Cuando el cliente solicita la cancelación. La devolución de la
+            misma solamente podrá solicitarse si no se ha pagado el importe correspondiente a Hacienda o a la DGT. En
+            cualquier caso, el cliente deberá aceptar los gastos administrativos derivados.
+          </Paragraph>
+        </Condition>
+
+        <Condition>
+          <h2>3. Descripción del servicio</h2>
+          <Paragraph>
+            3.1 AutoTrafic Gestoría tramitará la gestión vía gestor interno, DGT y organismos públicos o privados que
+            correspondan en aras de obtener el servicio que el cliente o usuario ha solicitado a través del sitio web.
+          </Paragraph>
+
+          <Paragraph>
+            3.2 Se deben enviar a AutoTrafic Gestoría los datos e información del vehículo, comprador y vendedor a través del
+            sitio web, correo electrónico, teléfono u otras soluciones electrónicas o de software compatibles con AutoTrafic
+            Gestoría. Una vez recibida la solicitud, AutoTrafic Gestoría comprobará la viabilidad de la solicitud.
+          </Paragraph>
+          <Paragraph>
+            3.3 AutoTrafic Gestoría se encarga de recoger la documentación oficial necesaria para la tramitación en la
+            dirección proporcionada por el cliente. Esta recogida incluye dos intentos, si en ninguno de éstos se logra
+            recoger los documentos, deberá ser el cliente quien asuma los costes del envío de dicha documentación.
+          </Paragraph>
+          <Paragraph>
+            3.4 AutoTrafic Gestoría se encarga de realizar el envío de la documentación oficial gestionada durante el
+            servicio. Ésta se envía en la dirección proporcionada por el cliente. Si éste no se encuentra en el momento de la
+            entrega, él será el responsable de recoger dicha documentación en la dirección de recogida que proporcione el
+            servicio de mensajería.
+          </Paragraph>
+        </Condition>
+
+        <Condition>
+          <h2>4. Honorarios y comisiones</h2>
+          <Paragraph>
+            4.1 No se proveerá por defecto ninguna factura por el servicio prestado y comisión cargada, pero puede ser
+            solicitada y enviada por correo electrónico.
+          </Paragraph>
+
+          <Paragraph>
+            4.2 AutoTrafic Gestoría no será responsable de ningún importe de compensación, daños o similar, si tiene un
+            impedimento para transferir el pago al cliente debido a un acontecimiento fuera de su control, incluyendo sin
+            limitación, a una problemática derivada de un proveedor, empresa u organismo público externo de AutoTrafic
+            Gestoría.
+          </Paragraph>
+          <Paragraph>
+            4.3 Puede existir una diferencia en el importe del impuesto al liquidarlo en hacienda, según la fecha exacta de
+            la primera matriculación. AutoTrafic Gestoría no cobra ninguna comisión sobre este importe, sino que liquida el
+            suplido aportado por el cliente en la misma administración.
+          </Paragraph>
+        </Condition>
+
+        <Condition>
+          <h2>5. Protección de datos</h2>
+          <Paragraph>
+            AutoTrafic Gestoría sólo utilizará los datos personales proporcionados por el cliente con la finalidad de
+            tramitar la solicitud de conformidad con la legislación vigente en España y en la Unión Europea, en particular,
+            con el Reglamento 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016, relativo a la protección
+            de las personas físicas en lo que respecta al tratamiento de datos personales y a la libre circulación de estos
+            datos y su normativa de desarrollo. El cliente proporciona a AutoTrafic Gestoría datos personales con el permiso
+            explícito para procesar los datos proporcionados y para el uso de los mismos en el contexto de la relación
+            contractual. AutoTrafic Gestoría sólo transferirá los datos personales a terceras partes bajo las siguientes
+            condiciones: Si es por una finalidad directamente relacionada con el objetivo original por el cual fueron
+            recopilados los datos. Si es requerido debido a una obligación legal o administrativa. Si sirven para la
+            prevención de la utilización incorrecta u otras actividades ilegales, tales como ataques deliberados, para
+            garantizar la seguridad de los datos.
+          </Paragraph>
+        </Condition>
+
+        <Condition>
+          <h2>6. Datos e información del cliente</h2>
+          <Paragraph>
+            6.1 El cliente debe proporcionar todos los datos e información necesaria para la correcta ejecución del servicio.
+            El cliente garantiza que los datos e información proporcionados son correctos, completos y ciertos.
+          </Paragraph>
+          <Paragraph>
+            6.2 En caso de datos e información incorrecta o conducta fraudulenta, AutoTrafic Gestoría se reserva el derecho a
+            finalizar el acuerdo con efecto inmediato. Si el acuerdo finaliza de conformidad con este párrafo, el cliente no
+            tendrá ningún derecho a compensación.
+          </Paragraph>
+        </Condition>
+
+        <Condition>
+          <h2>7. Política de desistimiento</h2>
+          <Paragraph>
+            7.1 El Cliente tendrá derecho a renunciar a la contratación antes de que transcurran 14 días naturales desde que
+            ésta se haya iniciado, siempre y cuando no haya empezado el trámite por parte de AutoTrafic Gestoría en la DGT.
+            Por este motivo, en AutoTrafic Gestoría ponemos a disposición de los Clientes un Formulario de Solicitud de
+            Devolución en este enlace. En ningún caso será posible solicitar la devolución en relación a los servicios de
+            INFORME DGT, DISTINTIVO AMBIENTAL y GARANTÍA MECÁNICA. Esto es debido a que estos trámites se inician de forma
+            automática y empiezan en el mismo momento que el Cliente realiza el pago en nuestra web. Por otro lado, los
+            servicios de NOTIFICACIÓN DE VENTA, ALTA/BAJA DE VEHÍCULO, DUPLICADO DEL PERMISO DE CIRCULACIÓN, MATRICULACIÓN DE
+            VEHÍCULOS y TRANSFERENCIA DE VEHÍCULOS, y pese a que la ejecución del servicio también se inicia de manera
+            automática y de forma inmediata, en AutoTrafic Gestoría ofrecemos la posibilidad al Cliente de renunciar al
+            contrato, siempre y cuando se cumplan las siguientes condiciones: En un plazo máximo de 14 días naturales, a
+            contar desde el momento en que se realizó el pago y aceptó las condiciones de servicio con AutoTrafic Gestoría,
+            el Cliente deberá enviar un Formulario de Solicitud de Devolución del trámite correspondiente. El Formulario de
+            Solicitud de Devolución habrá de estar firmada por las partes involucradas en el trámite o titular de la cuenta
+            con la que se ha formalizado dicho trámite. A estos efectos, es necesario que, por parte de AutoTrafic Gestoría,
+            nos sea posible validar las firmas, entendiéndose válidas aquellas firmas legitimadas por Notario o realizadas
+            mediante certificado electrónico reconocido*. *En caso de que la validez de las firmas no pueda ser verificada,
+            se considerará que no se ha cumplido la condición y no será posible proceder a la devolución del valor de la
+            contratación. El Formulario de Solicitud de Devolución, firmada según las condiciones anteriores, deberá enviarse
+            al correo electrónico siguiente: contacto@autotrafic.es. Además, tendrá que adjuntar el DNI del Titular de la
+            cuenta junto al Comprobante de Pago de los servicios contratados con AutoTrafic Gestoría. En dicho Comprobante,
+            tendrá que aparecer la cuenta bancaria en cuestión y el nombre del titular. De esta manera, validamos que la
+            devolución se realiza a la cuenta correspondiente.* *Si no dispone de una cuenta bancaria, no será posible
+            desistir de la contratación realizada. En el caso de TRANSFERENCIA DE VEHÍCULOS si ya se ha realizado el pago del
+            Impuesto de Transmisiones Patrimoniales en Hacienda y se ha presentado la documentación en la DGT no se devolverá
+            el importe cobrado.
+          </Paragraph>
+
+          <Paragraph>
+            7.2 Entendemos que el servicio se ha llevado a cabo cuando se haya iniciado la tramitación con la DGT. En ese
+            caso, se hace una retención de 40 euros, IVA incluido, en concepto de gastos de gestión que dicha contratación
+            haya conllevado en MATRICULACIÓN DE VEHÍCULOS y TRANSFERENCIA DE VEHÍCULOS; y, 20 euros, IVA incluido, en
+            NOTIFICACIÓN DE VENTA, ALTA/BAJA DE VEHÍCULO y DUPLICADO DEL PERMISO DE CIRCULACIÓN.
+          </Paragraph>
+
+          <Paragraph>
+            7.3 Una vez recibido el Formulario, AutoTrafic Gestoría enviará al Cliente un mail reportando si ha habido
+            aceptación o denegación del desistimiento o, en su caso, la necesidad de subsanación de la solicitud realizada.
+            Este mail se realizará en las 48 horas hábiles siguientes a la recepción de la solicitud de devolución enviada
+            por el Cliente.
+          </Paragraph>
+          <Paragraph>
+            7.4 Dada la propia naturaleza del servicio, el Cliente es consciente que cuando el servicio haya sido ejecutado,
+            no podrá desistir de forma gratuita de conformidad con el artículo 16 de la Directiva 2011/83/UE.
+          </Paragraph>
+
+          <Paragraph>
+            7.5. La realización del pago de la devolución será a finales del mes vencido a la presentación de la solicitud de
+            devolución del Cliente.
+          </Paragraph>
+        </Condition>
+
+        <Condition>
+          <h2>8. Disposiciones finales</h2>
+
+          <Paragraph>
+            8.1 AutoTrafic Gestoría está autorizada a modificar estos Términos y Condiciones, honorarios, comisiones, y a
+            establecer condiciones adicionales en cualquier momento y sin notificación. No obstante, aquellos cambios que
+            conlleven un efecto negativo al cliente no se aplicarán a este a no ser que acceda a dichos nuevos cambios.
+          </Paragraph>
+
+          <Paragraph>
+            8.2 Serán aplicables las leyes de España a estos Términos y Condiciones, al documento de autorización y al
+            acuerdo entre AutoTrafic Gestoría y el cliente. Cualquier conflicto en relación a estos Términos y Condiciones,
+            al documento de autorización y cesión de derechos o a disposiciones del sitio web, deberá ser resuelto
+            exclusivamente ante los tribunales de Barcelona.
+          </Paragraph>
+
+          <Paragraph>
+            8.3 Si alguna disposición de estos Términos y Condiciones fuese o se convirtiese en nula, ilegal o inaplicable,
+            ello no afectará a la validez de las disposiciones restantes.
+          </Paragraph>
+        </Condition>
+      </Section>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 70px;
+`;
+
+const Section = styled.div`
+  max-width: 800px;
+`;
+
+const Condition = styled.div`
+  margin-bottom: 100px;
+`;
+
+const Paragraph = styled.div`
+  margin: 20px 0;
+`;
