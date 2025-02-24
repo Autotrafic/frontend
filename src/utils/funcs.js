47fdc34c54ab9f8634f4c55f1b1d3e5aaecdbb4f
unknown <aronilie.code@gmail.com>
2025-02-24 10:22:54 +0100

    Updated whatsapp click function

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -1,11 +1,13 @@
 import { logWhatsappClick } from "../services/logger";
 
 export const onWhatsappClick = (e) => {
-    e.preventDefault();
-  
-    window.open('https://wa.me/34643219297', '_blank');
-  
-    logWhatsappClick().catch((error) => {
-      console.error('Failed to log WhatsApp click:', error);
-    });
-  };
+  e.preventDefault();
+
+  const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+  window.open('https://wa.me/34643219297', isIOS ? '_self' : '_blank');
+
+  logWhatsappClick().catch((error) => {
+    console.error('Failed to log WhatsApp click:', error);
+  });
+};
